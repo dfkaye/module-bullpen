08ffdabe6c82ed39af340c2c2c8faf43638fd998
david kaye <dfkaye@yahoo.com>
2013-08-26 11:42:50 -0700

    Update module.js

--- js/module.js
+++ js/module.js
@@ -1,8 +1,20 @@
 typeof global != 'undefined' || (window.global = window);
-global.console && console.log('module.js');
+
+var dirname;
+var filename = '/module.js';
+var scripts = document.scripts;
+var length = scripts.length;
+for (var i = 0; i < length; ++i) {
+  if (scripts.src.indexOf(filename) > 0 )
+    dirname = scripts.src.split(filename)[0];
+    break;
+}
+
+global.console && console.log(dirname + 'module.js');
+
 
 function define(name) {
-  
+  global.module = new Module(name);
 }
 
 function expand(path, parent) {
@@ -18,3 +30,15 @@ require.cache = {};
 function load(path, callback) {
   callback(err, parent)
 }
+
+function Module(id) {
+  
+  this.id = id;
+  this.filename = id + '.js';
+  this.exports = {};
+  
+}
+
+define('.');
+
+define('./inline');
