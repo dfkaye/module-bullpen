a2a438ff38db2e118ff094e881e6d4442404bcc2
david kaye <dfkaye@yahoo.com>
2013-08-26 14:32:39 -0700

    Update module.js

--- js/module.js
+++ js/module.js
@@ -3,6 +3,8 @@ typeof global != 'undefined' || (window.global = window);
 var dirname;
 var filename = '/module.js';
 var scripts = document.scripts;
+var head = scripts[0].parentNode
+
 var length = scripts.length;
 var src;
 for (var i = 0; i < length; ++i) {
@@ -29,23 +31,50 @@ function expand(path, parent) {
 function require(id, name) {
   path = expand(id);
   if (!require.cache[path]) {
-   console.log('not found: ' + path) 
+    console.log('not found: ' + path);
+    load(path);
   }
   return require.cache[path].exports;
 }
 
 require.cache = {};
 
-function load(path, callback) {
-  callback(err, parent)
+
+function load(url, callback) {
+  //callback(err, parent)
+  url = expand(url);
+  
+  var script = document.createElement('script');
+  
+  script.src = url;
+  script.onload = script.onreadystatechange = function () {
+    if (!script.readyState || script.readyState.match(/loaded|complete/i)) {
+      script.onload = script.onreadystatechange = null;
+      resolve(url);
+    }
+  }
+  
+  script.onerror = function (e) {
+    console.error('error loading script: ' + e);
+    //module.pathError(filename);
+  }
+  
+  head.appendChild(script);
 }
+
+function resolve(url) {
+  
+  console.log('url loaded: ' + url)
+}
+
 
 function Module(id) {
   
   this.id = id;
   this.filename = id + '.js';
   this.exports = {};
-  
+  this.children = [];
+  this.parent;
 }
 
 
