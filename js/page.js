ce0e9c3f3fac577dfd66320c3037958b7720b947
david kaye <dfkaye@yahoo.com>
2013-08-26 11:32:48 -0700

    Update page.js

--- js/page.js
+++ js/page.js
@@ -1,1 +1,18 @@
-global.console && console.log('page.js');
+define('./page.js');
+require('./inline');
+require('./remote');
+
+;(function() {
+  
+  var inline, remote;
+  
+  inline = require('inline');
+  remote = require('remote');
+
+  module.exports = s; function s() {
+    
+  }
+  
+  global.console && console.log('page.js');
+
+}());
