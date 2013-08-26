c823332a058a9606dbcc6366d9897971a35c7784
david kaye <dfkaye@yahoo.com>
2013-08-26 11:11:23 -0700

    Update module.js

--- module.js
+++ module.js
@@ -1,1 +1,17 @@
-typeof console != 'undefined' && console.log('module.js');
+typeof global != 'undefined' || (window.global = window);
+global.console && console.log('module.js');
+
+function expand(path, parent) {
+  
+}
+
+function require(path, name) {
+  
+}
+
+require.cache = {};
+
+function load(path, callback) {
+  callback(err, parent)
+}
+
